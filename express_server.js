f9259e91a47055a512b797c07c4dee0fdfef43e6
emtupp <emilyc.tupper@gmail.com>
2020-11-10 01:39:27 +0000

    modified express_server to be able to post a new longURL

--- express_server.js
+++ express_server.js
@@ -1,13 +1,31 @@
+const generateRandomString = function() {
+  let formOutput = Math.random().toString(36).substring(2,8);
+  return formOutput;
+};
+
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
 
 app.get("/", (req, res) => {
   res.send("Hello!");
